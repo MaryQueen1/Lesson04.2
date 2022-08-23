28d19b55d28f8364c024f267a00755d040bb4a99
Mary <maryqueenk1@gmail.com>
2022-08-23 18:52:09 +0300

    Задача 41. Сколько чисел больше 0

---
+++ HW6Ex01/Program.cs
@@ -0,0 +1,54 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.WriteLine("Задача 41.");
+
+int[] CreateArray()
+{
+    Console.WriteLine("Введите числа: ");
+    string? array = Console.ReadLine();
+    array = array.Replace(",", "");
+    array = array.Replace(".", "");
+
+    string[] ar = array.Split(" ");
+    int[] arr = new int[ar.Length];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(ar[i]);
+    }
+    return arr;
+}
+
+int[] Numbers = CreateArray();
+
+void More(int[] M)
+{
+    int result = 0;
+    for (int i = 0; i < M.Length; i++)
+    {
+        if (M[i] > 0)
+        {
+            result = result + 1;
+        }
+        else
+        {
+            result = result + 0;
+        }
+    }
+    if(result == 1)
+    {
+    System.Console.WriteLine($"Вы ввели {result} число больше 0");
+    }
+    else if(result == 2 || result == 3 || result == 4)
+    {
+    System.Console.WriteLine($"Вы ввели {result} числа больше 0");
+    }
+    else
+    {
+    System.Console.WriteLine($"Вы ввели {result} чисел больше 0");
+    }
+}
+
+More(Numbers);
---
+++ L4Ex03/Program.cs
@@ -0,0 +1,11 @@
+﻿int Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n ==1) return 1;
+    else return n * Factorial(n-1);
+}
+
+System.Console.WriteLine(Factorial(5));
+
+// но если это int то из-за переполнения с 17! будет неверный вывод
